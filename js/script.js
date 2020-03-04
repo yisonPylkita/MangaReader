89303ec3d71e3d0eefdefff2c4eb3778ba65848a
Wojciech Bartnik <wbartnik@vewd.com>
2020-03-04 11:29:54 +0100

    Basic manga cover representation

--- js/script.js
+++ js/script.js
@@ -3,20 +3,29 @@ class MangaTile {
         this.name = name;
         this.cover_img = cover_img;
     }
+
+    toString1() {
+        return `<dev class="MangaTile">
+            <dev class="MangaTileCover" style="background-image: url(${this.cover_img})"></div>
+            <dev class="MangaTileTitle">${this.name}</div>
+        </dev>`;
+    }
 }
 
 const fav_mangas_mock = [
     new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
     new MangaTile('Hidan no Aria', 'https://cdn1.mangafox.online/119/131/088/136/610/2/hidan-no-aria.jpg'),
-    new MangaTile('Ominous Liar', ''),
-    new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
-    new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
+    new MangaTile('Flower War', 'https://cdn1.mangafox.online/114/720/144/776/314/2/flower-war.jpg'),
 ]
 
-let render_fav_page = () => {
-
+let renderFavPage = () => {
+    const favs = document.getElementById('favorite_manga');
+    for (const manga of fav_mangas_mock) {
+        favs.innerHTML += manga.toString1();
+    }
 }
 
 window.onload = () => {
     console.log('Helo there');
+    renderFavPage();
 }
