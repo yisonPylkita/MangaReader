cb07ffdbcd90d08f449106854e1f69070493cffd
Wojciech Bartnik <wbartnik@vewd.com>
2020-03-04 16:29:06 +0100

    More progress on layout

--- js/script.js
+++ js/script.js
@@ -6,13 +6,24 @@ class MangaTile {
 
     toString() {
         return `<dev class="MangaTile">
-            <dev class="MangaTileCover" style="background-image: url(${this.cover_img})"></div>
+            <img class="MangaTileCover" src="${this.cover_img}"></img>
             <dev class="MangaTileTitle">${this.name}</div>
         </dev>`;
+
+        // return `<dev class="MangaTileCover" style="background-image: url(${this.cover_img})"></div>`;
     }
 }
 
 const fav_mangas_mock = [
+    new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
+    new MangaTile('Hidan no Aria', 'https://cdn1.mangafox.online/119/131/088/136/610/2/hidan-no-aria.jpg'),
+    new MangaTile('Flower War', 'https://cdn1.mangafox.online/114/720/144/776/314/2/flower-war.jpg'),
+    new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
+    new MangaTile('Hidan no Aria', 'https://cdn1.mangafox.online/119/131/088/136/610/2/hidan-no-aria.jpg'),
+    new MangaTile('Flower War', 'https://cdn1.mangafox.online/114/720/144/776/314/2/flower-war.jpg'),
+    new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
+    new MangaTile('Hidan no Aria', 'https://cdn1.mangafox.online/119/131/088/136/610/2/hidan-no-aria.jpg'),
+    new MangaTile('Flower War', 'https://cdn1.mangafox.online/114/720/144/776/314/2/flower-war.jpg'),
     new MangaTile('Peerless Refiner', 'https://cdn1.mangafox.online/307/407/737/548/280/peerless-refiner.jpg'),
     new MangaTile('Hidan no Aria', 'https://cdn1.mangafox.online/119/131/088/136/610/2/hidan-no-aria.jpg'),
     new MangaTile('Flower War', 'https://cdn1.mangafox.online/114/720/144/776/314/2/flower-war.jpg'),
